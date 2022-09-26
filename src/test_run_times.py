2977d322b0da34c7849aa055074d9fd7d209341d
azerata <smedemarkhammer@gmail.com>
2022-09-26 13:59:23 +0200

    updated testing

--- src/test_run_times.py
+++ src/test_run_times.py
@@ -36,21 +36,20 @@ def time_naive(genome: dict[str, str], reads: dict[str, str]) -> pd.DataFrame:
     return out
 
 
-if __name__ == '__main__':
-    main()
-
-sys.exit()
-
-
-def time_linear():
+def time_linear(genome: dict[str, str], reads: dict[str, str]) -> pd.DataFrame:
     n, m, time = [], [], []
     for chr in genome:
         for read in reads:
+            print(f"processing {chr} {read}")
             n.append(len(genome[chr]))
             m.append(len(reads[read]))
-            cmd = f'linear({genome[chr]}, {reads[read]})'
-            ti = timeit.timeit(cmd, setup="from lin import linear", number=10)
+            cmd = f'linear("{genome[chr]}", "{reads[read]}")'
+            print(cmd)
+            ti = timeit.timeit(cmd, setup="from linear import linear")
+            time.append(ti)
+    out = pd.DataFrame({"n size": n, "m size": m, "time": time})
+    return out
 
-    ti = timeit.timeit()
 
-    linear()
+if __name__ == '__main__':
+    main()
